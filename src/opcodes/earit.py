cf366f6326758dd9aa22c29cc3bf0f12d004425b
Pablok98 <pjkipreos@uc.cl>
2021-10-28 16:22:08 -0300

    Bitwise arithmetic progress

--- src/opcodes/earit.py
+++ src/opcodes/earit.py
@@ -1,170 +1,337 @@
-# TODO: set flags
+# AUXILIARY
+# TODO: optimize flag getter
+def _lbin_to_hex(list_):
+    string = ''.join([str(x) for x in list_])
+    numb = int(string, 2)
+    return hex(numb)
+
+
+def _hex_to_lbin(hex_):
+    bin_ = bin(hex_)[2:].zfill(8)
+    list_ = [int(x) for x in bin_]
+    return list_
+
+
+# ******   ADD   **********
+def add_op(cpu, value, carry=False):
+    if carry:
+        carry = _hex_to_lbin(cpu.registers['F'])[4]
+        value += carry
+    new_value = cpu.registers['A'] + value
+    flag = [0, 0, 0, 0, 0, 0, 0, 0]
+    if (((cpu.registers['A'] & 0xf) + (value & 0xf)) & 0x10) == 0x10:
+        flag[5] = 1
+    if new_value >= 256:
+        new_value -= 256
+        flag[4] = 1
+    cpu.registers['A'] = new_value
+    if new_value == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
+
+
 def add_ar(cpu, r):
     # Add A,r
     # z0hc
-    pass
+    add_op(cpu, cpu.registers[r])
 
 
 def add_an(cpu, n):
     # Add A,n
     # z0hc
-    pass
+    add_op(cpu, n)
 
 
 def add_ahl(cpu):
     # Add A,(HL)
     # z0hc
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    add_op(cpu, value)
 
 
+# ******   ADC   **********
 def adc_ar(cpu, r):
     # adc A,r
     # z0hc
-    pass
+    add_op(cpu, cpu.registers[r], True)
 
 
 def adc_an(cpu, n):
     # adc A,n
     # z0hc
-    pass
+    add_op(cpu, n, True)
 
 
 def adc_ahl(cpu):
     # adc A,(HL)
     # z0hc
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    add_op(cpu, value, True)
+
+
+# ***  Subtraction  ****
+def sub_op(cpu, value, carry=False):
+    if carry:
+        carry = _hex_to_lbin(cpu.registers['F'])[4]
+        value += carry
+    new_value = cpu.registers['A'] - value
+    flag = [0, 0, 0, 0, 0, 0, 1, 0]
+    if (((cpu.registers['A'] & 0xf) - (value & 0xf)) & 0x10) == 0x10:
+        flag[5] = 1
+    if new_value < 0:
+        new_value += 256
+        flag[4] = 1
+    cpu.registers['A'] = new_value
+    if new_value == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def sub_ar(cpu, r):
     # sub A,r
     # z1hc
-    pass
+    sub_op(cpu, cpu.registers[r])
 
 
 def sub_an(cpu, n):
     # sub A,n
     # z1hc
-    pass
+    sub_op(cpu, n)
 
 
 def sub_ahl(cpu):
     # sub A,(HL)
     # z1hc
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    sub_op(cpu, value)
 
 
 def sbc_ar(cpu, r):
     # sbc A,r
     # z1hc
-    pass
+    sub_op(cpu, cpu.registers[r], True)
 
 
 def sbc_an(cpu, n):
     # sbc A,n
     # z1hc
-    pass
+    sub_op(cpu, n, True)
 
 
 def sbc_ahl(cpu):
     # sbc A,(HL)
     # z1hc
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    sub_op(cpu, value, True)
+
+
+# *********  AND  *********
+def and_op(cpu, value):
+    new_value = cpu.registers['A'] & value
+    cpu.registers['A'] = new_value
+    flag = [0, 0, 0, 0, 0, 1, 0, 0]
+    if new_value == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def and_ar(cpu, r):
     # and A,r
     # z010
-    pass
+    and_op(cpu, cpu.registers[r])
 
 
 def and_an(cpu, n):
     # and A,n
     # z010
-    pass
+    and_op(cpu, n)
 
 
 def and_ahl(cpu):
     # and A,(HL)
     # z010
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    and_op(cpu, value)
+
+
+# *** XOR
+def xor_op(cpu, value):
+    new_value = cpu.registers['A'] ^ value
+    cpu.registers['A'] = new_value
+    flag = [0, 0, 0, 0, 0, 0, 0, 0]
+    if new_value == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def xor_ar(cpu, r):
     # xor A,r
     # z000
-    pass
+    xor_op(cpu, cpu.registers[r])
 
 
 def xor_an(cpu, n):
     # xor A,n
     # z000
-    pass
+    xor_op(cpu, n)
 
 
 def xor_ahl(cpu):
     # xor A,(HL)
     # z000
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    xor_op(cpu, value)
+
+
+# *** OR
+def or_op(cpu, value):
+    new_value = cpu.registers['A'] | value
+    cpu.registers['A'] = new_value
+    flag = [0, 0, 0, 0, 0, 0, 0, 0]
+    if new_value == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def or_ar(cpu, r):
     # or A,r
     # z000
-    pass
+    or_op(cpu, cpu.registers[r])
 
 
 def or_an(cpu, n):
     # or A,n
     # z000
-    pass
+    or_op(cpu, n)
 
 
 def or_ahl(cpu):
     # or A,(HL)
     # z000
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    or_op(cpu, value)
+
+
+# ****  Compare  ****
+def cp_op(cpu, value):
+    new_value = cpu.registers['A'] - value
+    flag = [0, 0, 0, 0, 0, 0, 1, 0]
+    if (((cpu.registers['A'] & 0xf) - (value & 0xf)) & 0x10) == 0x10:
+        flag[5] = 1
+    if new_value <= 0:
+        new_value += 256
+        flag[4] = 1
+    if new_value == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def cp_ar(cpu, r):
     # cp A,r
     # z1hc
-    pass
+    cp_op(cpu, cpu.registers[r])
 
 
 def cp_an(cpu, n):
     # cp A,n
     # z1hc
-    pass
+    cp_op(cpu, n)
 
 
 def cp_ahl(cpu):
     # cp A,(HL)
     # z1hc
-    pass
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    value = cpu.memory.read_data(first + second)
+    cp_op(cpu, value)
 
 
+# ** OTHER **
 def inc_r(cpu, r):
     # inc r
     # z0h-
-    pass
+    flag = _hex_to_lbin(cpu.registers['F'])[4]
+    flag = [0, 0, 0, 0, flag, 0, 0, 0]
+    if (((cpu.registers[r] & 0xf) + (1 & 0xf)) & 0x10) == 0x10:
+        flag[5] = 1
+    cpu.registers[r] += 1
+    if cpu.registers[r] >= 256:
+        cpu.registers[r] -= 256
+    if cpu.registers[r] == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def inc_hl(cpu):
     # inc (HL)
     # z0h-
-    pass
+    flag = _hex_to_lbin(cpu.registers['F'])[4]
+    flag = [0, 0, 0, 0, flag, 0, 0, 0]
+
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    address = first + second
+    value = cpu.memory.read_data(address)
+    if (((value & 0xf) + (1 & 0xf)) & 0x10) == 0x10:
+        flag[5] = 1
+    value += 1
+    if value >= 256:
+        value -= 256
+    if value == 0:
+        flag[7] = 1
+    cpu.memory.write_data(value, address)
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def dec_r(cpu, r):
     # dec r
     # z1h-
-    pass
+    flag = _hex_to_lbin(cpu.registers['F'])[4]
+    flag = [0, 0, 0, 0, flag, 0, 1, 0]
+    if (((cpu.registers[r] & 0xf) - (1 & 0xf)) & 0x10) == 0x10:
+        flag[5] = 1
+    cpu.registers[r] -= 1
+    if cpu.registers[r] < 0:
+        cpu.registers[r] += 256
+    if cpu.registers[r] == 0:
+        flag[7] = 1
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def dec_hl(cpu):
     # dec (HL)
     # z1h-
-    pass
+    flag = _hex_to_lbin(cpu.registers['F'])[4]
+    flag = [0, 0, 0, 0, flag, 0, 1, 0]
+
+    first = (cpu.registers['H'] << 8) & 0xFF00
+    second = cpu.registers['L']
+    address = first + second
+    value = cpu.memory.read_data(address)
+    if (((value & 0xf) - (1 & 0xf)) & 0x10) == 0x10:
+        flag[5] = 1
+    value -= 1
+    if value < 0:
+        value += 256
+    if value == 0:
+        flag[7] = 1
+    cpu.memory.write_data(value, address)
+    cpu.registers['F'] = _lbin_to_hex(flag)
 
 
 def daa(cpu):
